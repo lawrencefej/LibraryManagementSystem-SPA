6ed5111b04b7ac17af01dd796789668f108b746a
Lawrence <lawrencefej@gmail.com>
2018-12-20 19:21:29 -0500

    Configured login feature

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseurl = environment.apiUrl + 'auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
+  currentUser: User;
+  photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+
+constructor(private http: HttpClient) { }
+
+login(model: any) {
+  return this.http
+  .post(this.baseurl + 'login', model)
+  .pipe(
+    map(( response: any) => {
+      const user = response;
+      if (user) {
+        localStorage.setItem('token', user.token);
+        localStorage.setItem('user', JSON.stringify(user.user));
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        this.currentUser = user.user;
+      }
+    })
+  );
+}
+
+register(user: User) {
+  return this.http.post(this.baseurl + 'register', user);
+}
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
+}
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { JwtModule } from '@auth0/angular-jwt';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AuthService } from './_services/auth.service';
+import { ErrorinterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+
+export function tokenGetter() {
+   return localStorage.getItem('token');
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent,
+      RegisterComponent,
+      LoginComponent,
+      HomeComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      FormsModule,
+      AppRoutingModule,
+      BsDropdownModule.forRoot(),
+      JwtModule.forRoot({
+         config: {
+             tokenGetter: tokenGetter,
+             whitelistedDomains: ['localhost:5000'],
+             blacklistedRoutes: ['localhost:5000/auth/']
+         }
+     })
+   ],
+   providers: [
+      AuthService,
+      ErrorinterceptorProvider,
+      AlertifyService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
