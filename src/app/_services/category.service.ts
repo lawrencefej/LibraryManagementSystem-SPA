d752d3ea580de782596a153e0220cb1a75b5c1d5
Lawrence <lawrencefej@gmail.com>
2019-03-10 14:56:00 -0400

    Added category service

--- src/app/_services/category.service.ts
+++ src/app/_services/category.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { AssetType } from '../_models/assetType';
 import { Observable } from 'rxjs';
+import { Category } from '../_models/category';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +12,12 @@ export class CategoryService {
 
 constructor(private http: HttpClient) { }
 
-  getAssetType(id): Observable<AssetType> {
-    return this.http.get<AssetType>(this.baseUrl + 'assettype/' + id);
+  getCategory(id): Observable<Category> {
+    return this.http.get<Category>(this.baseUrl + 'category/' + id);
   }
 
-  getAssetTypes(): Observable<AssetType[]> {
-    return this.http.get<AssetType[]>(this.baseUrl + 'assettype/');
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.baseUrl + 'category/');
   }
 
 }
