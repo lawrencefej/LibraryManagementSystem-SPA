9202ab974a8b75f042630b38480910e8b6bcd4e4
Lawrence <lawrencefej@gmail.com>
2019-03-16 14:44:03 -0400

    created the dashboard layout

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  TotalMembers: number = 2000;
+  TotalItems: number = 10000;
+  TotalAuthors: number = 2000;
+  TotalCheckouts: number = 2000;
 
   constructor() { }
 
