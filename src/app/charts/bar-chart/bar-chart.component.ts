b038de04587b4cfe177992e8400d15023db628b2
Lawrence <lawrencefej@gmail.com>
2019-03-17 16:26:19 -0400

    Resized charts and changed sample data

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -19,14 +19,14 @@ export class BarChartComponent implements OnInit {
       }
     }
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: Label[] = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   // public barChartPlugins = [pluginDataLabels];
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Checkouts' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Returns' }
   ];
 
   constructor() { }
--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -9,13 +9,14 @@ import { Label, Color } from 'ng2-charts';
 })
 export class LineChartComponent implements OnInit {
   public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Checkouts' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Returns' },
+    // { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }
   ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartLabels: Label[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   public lineChartOptions: (ChartOptions & { annotation: any }) = {
     responsive: true,
+    maintainAspectRatio: false,
     scales: {
       // We use this empty structure as a placeholder for dynamic theming.
       xAxes: [{}],
