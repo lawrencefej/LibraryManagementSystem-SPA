af8ab9ab6abea231c294b6ff010124dce48e0276
Lawrence <lawrencefej@gmail.com>
2019-03-23 12:43:22 -0400

    Created Chart Models

---
+++ src/app/_models/chartData.ts
@@ -0,0 +1,7 @@
+export interface ChartData {
+  data: number;
+  name: string;
+  date: Date;
+  day: string;
+  month: number;
+}
---
+++ src/app/_models/chartModel.ts
@@ -0,0 +1,5 @@
+import { ChartData } from './chartData';
+export interface ChartModel {
+  data: ChartData[];
+  label: string;
+}
