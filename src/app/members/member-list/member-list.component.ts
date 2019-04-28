2c91eea94d51664effff1bb930d9c696309aff1d
Lawrence Fejokwu <lawrencefej@gmail.com>
2019-04-27 20:31:26 -0400

    Added pagination for member list

--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -7,6 +7,7 @@ import { AuthService } from 'src/app/_services/auth.service';
 import { MemberEditComponent } from '../member-edit/member-edit.component';
 import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
+import { Pagination, PaginatedResult } from 'src/app/_models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -19,32 +20,45 @@ export class MemberListComponent implements OnInit {
   count: number;
   value = '';
   bsModalRef: BsModalRef;
+  pagination: Pagination;
 
-
-  constructor(private route: ActivatedRoute, private userService: UserService,
-     private alertify: AlertifyService, private modalService: BsModalService, private authService: AuthService, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private alertify: AlertifyService,
+    private modalService: BsModalService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.members = data['members'];
+      this.members = data['members'].result;
+      this.pagination = data['members'].pagination;
       this.count = this.members.length;
     });
+    console.log(this.members);
   }
 
   searchMembers(value: string) {
-    this.userService.searchMembers(value).subscribe((members: User[]) => {
-      this.members = members;
-      this.count = this.members.length;
-    }, error => {
-      this.alertify.error(error);
-    });
+    this.userService.searchMembers(value).subscribe(
+      (members: User[]) => {
+        this.members = members;
+        this.count = this.members.length;
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
   editUserModal(member: User) {
     const initialState = {
       member
     };
-    this.bsModalRef = this.modalService.show(MemberEditComponent, {initialState});
+    this.bsModalRef = this.modalService.show(MemberEditComponent, {
+      initialState
+    });
     this.bsModalRef.content.updatedMember.subscribe((value: User) => {
       this.updateUser(value);
     });
@@ -58,21 +72,29 @@ export class MemberListComponent implements OnInit {
   }
 
   addMember(value: User) {
-    this.userService.AddMember(value).subscribe((member: User) => {
-      this.alertify.success('Member Added Successfully');
-      this.members.unshift(member);
-      this.router.navigate(['/members/', member.id]);
-    }, error => {
-      this.alertify.error(error);
-    });
+    this.userService.AddMember(value).subscribe(
+      (member: User) => {
+        this.alertify.success('Member Added Successfully');
+        this.members.unshift(member);
+        this.router.navigate(['/members/', member.id]);
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
   updateUser(member: User) {
-    this.userService.updateUser(this.authService.decodedToken.nameid, member).subscribe(next => {
-      this.alertify.success('Member Updated Successfully');
-    }, error => {
-      this.alertify.error(error);
-    });
+    this.userService
+      .updateUser(this.authService.decodedToken.nameid, member)
+      .subscribe(
+        next => {
+          this.alertify.success('Member Updated Successfully');
+        },
+        error => {
+          this.alertify.error(error);
+        }
+      );
   }
 
   deleteMember(id) {
@@ -81,11 +103,35 @@ export class MemberListComponent implements OnInit {
   }
 
   getMember(id) {
-    this.userService.getUser(id).subscribe((member: User) => {
-      this.editUserModal(member);
-    }, error => {
-      this.alertify.error(error);
-    });
+    this.userService.getUser(id).subscribe(
+      (member: User) => {
+        this.editUserModal(member);
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.userService
+      .getPaginatedMembers(
+        this.pagination.currentPage,
+        this.pagination.itemsPerPage
+      )
+      .subscribe(
+        (res: PaginatedResult<User[]>) => {
+          this.members = res.result;
+          this.pagination = res.pagination;
+        },
+        error => {
+          this.alertify.error(error);
+        }
+      );
+  }
 }
