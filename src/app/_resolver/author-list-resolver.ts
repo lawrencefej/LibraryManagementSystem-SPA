5b00820ab6f060adaffc18f80d46fc44bff1c501
Lawrence Fejokwu <lawrencefej@gmail.com>
2019-04-27 21:39:30 -0400

    Added Pagination to author

--- src/app/_resolver/author-list-resolver.ts
+++ src/app/_resolver/author-list-resolver.ts
@@ -8,11 +8,13 @@ import { Author } from '../_models/author';
 
 @Injectable()
 export class AuthorListResolver implements Resolve<Author[]> {
+  pageNumber = 1;
+  pageSize = 5;
   constructor(private authorService: AuthorService,
     private router: Router, private alertify: AlertifyService) { }
 
   resolve(route: ActivatedRouteSnapshot): Observable<Author[]> {
-    return this.authorService.getAuthors().pipe(
+    return this.authorService.getPaginatedAuthors(this.pageNumber, this.pageSize).pipe(
       catchError(error => {
         this.alertify.error('Problem retrieving data');
         this.router.navigate(['/home']);
